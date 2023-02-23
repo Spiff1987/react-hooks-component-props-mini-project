70babbc7e3fa66bce4fcc57e64d5ff5eb92c888b
dismas <afubwa@gmail.com>
2023-02-23 12:16:51 +0400

    React hooks props mini project

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./Header";
+import About from "./About";
+import Article from "./Article";
+import ArticleList from "./ArticleList";
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+        <Header />
+      <aside>
+        <About />
+      </aside>
+      <main>
+        <ArticleList />
+      </main>
+
     </div>
   );
 }
 
 export default App;
+
+
+
